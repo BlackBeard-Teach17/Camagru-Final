0ab48ff2f1afc7f093b5ec836565c215fb6794fa
Keamogetswe Mfoloe <keamogetswemfoloe@gmail.com>
2020-08-04 06:45:51 +0200

    Updated the dev env db password

--- BlackBeard/Config/database.php
+++ BlackBeard/Config/database.php
@@ -4,6 +4,6 @@
     $DB_DSN = "mysql:host=127.0.0.1;dbname=".$DB_NAME;
     $DB_DSN1 = "mysql:host=127.0.0.1";
     $DB_USER = "root";
-    $DB_PASSWORD = "TaksForce141";
+    $DB_PASSWORD = "TaskForce141";
 
 ?>

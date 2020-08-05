b30d183f3a4408a529b95436de82abd018a52ec6
Keamogetswe Mfoloe <keamogetswemfoloe@gmail.com>
2020-08-05 12:58:42 +0200

    fixed bad urls

--- BlackBeard/functions/signup.php
+++ BlackBeard/functions/signup.php
@@ -15,54 +15,54 @@ $_SESSION['error'] = null;
 if(empty($mail) || empty($username) || empty($pwd) || empty($cpwd))
 {
     $_SESSION['error'] = "Field/s cannot be empty";
-    header("Location: ../views/index.php");
+    header("Location: ../index.php");
     exit();
 }
 if (!filter_var($mail, FILTER_VALIDATE_EMAIL))
 {
     $_SESSION['error'] = "Enter a valid Email";
-    header("Location: ../views/index.php");
+    header("Location: ../index.php");
     exit();
 }
 if (!preg_match("#[a-zA-Z]+#", $pwd))
 {
     $_SESSION['error'] = "Password shoud contain at least Lowercase, Uppercase";
-    header("Location: ../views/index.php");
+    header("Location: ../index.php");
     exit();
 }
 if (!preg_match("#[0-9]+#", $pwd))
 {
     $_SESSION['error'] = "Password shoud contain at least a digit ";
-    header("Location: ../views/index.php");
+    header("Location: ../index.php");
     exit();
 }
 if (strlen($pwd) < 4)
 {
     $_SESSION['error'] = "Password should contain at least 4 chars";
-    header("Location: ../views/index.php");
+    header("Location: ../index.php");
     exit();
 }
 if (strlen($username) < 4)
 {
     $_SESSION['error'] = "username should contain at least 4 chars";
-    header('Location: ../views/index.php');
+    header('Location: ../index.php');
     exit();
 }
 if (!preg_match("/^[a-zA-Z0-9]{3,}$/", $username)) 
 {
     $_SESSION['error'] = "Username should cocntain Lower/Upper case and should have atleast 3 chars";
-    header('Location: ../views/index.php');
+    header('Location: ../index.php');
     exit();
 }
 if ($pwd != $cpwd)
 {
     $_SESSION['error'] = "Passwords dont match try again";
-    header('Location: ../views/index.php');
+    header('Location: ../index.php');
     exit();
 }
 $url = $_SERVER['HTTP_HOST'] . str_replace("signup.php", "", $_SERVER['REQUEST_URI']);
 
 verify_signup($mail, $username, $pwd, $url);
 
-header("Location: ../views/index.php");
+header("Location: ../index.php");
 ?>
